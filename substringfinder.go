350f48f09bf6ed41688a20ef1b30faac3509f4f4
Pieter Vantorre <pietervantorre@gmail.com>
2014-06-24 01:07:42 +0200

    Find word and find data between 2 runes have been added

---
+++ substringfinder.go
@@ -0,0 +1,35 @@
+package substringfinder
+
+import "strings"
+
+func FindFirstOfSubString(parent, substring string) (int, int) {
+	return FindFirstOfSubStringWithStartingIndex(parent, substring, 0)
+}
+
+func FindFirstOfSubStringWithStartingIndex(parent, substring string, startingIndex int) (int, int) {
+	for i := startingIndex; i < len(parent) - len(substring); i++ {
+		foundSubString := parent[i] == substring[0]
+		for counter := 1; counter < len(substring); counter++ {
+			if foundSubString == false {
+				break
+			}
+			foundSubString =  parent[i + counter] == substring[counter]
+		}
+		if foundSubString	 == true {
+			return i, i + len(substring) - 1
+		}
+	}
+	return -1, -1
+}
+
+func FindIndicesBetweenRunes(parent string, leftRune, rightRune rune) (int, int) {
+	return FindIndicesBetweenRunesWithStartingIndex(parent, leftRune, rightRune, 0)
+}
+
+func FindIndicesBetweenRunesWithStartingIndex(parent string, leftRune, rightRune rune, startingIndex int) (int, int) {
+	substring := parent[startingIndex:len(parent)]
+	lboundIndex := strings.IndexRune(substring, leftRune)
+	substring = parent[startingIndex + lboundIndex: len(parent)]
+	rboundIndex := strings.IndexRune(substring, rightRune)
+	return lboundIndex + startingIndex, rboundIndex + lboundIndex + startingIndex
+}
