cee26c5e0017a2d33a7be4e37a63fe4f32f86083
Pieter Vantorre <pietervantorre@gmail.com>
2014-06-24 13:52:33 +0200

    Added functionality to find matching runes (like brackets) so that you can find the correct closing rune. Also included ignore comment / string text behaviour

--- substringfinder.go
+++ substringfinder.go
@@ -1,6 +1,11 @@
 package substringfinder
 
-import "strings"
+import (
+	"strings"
+
+//"math"
+//"fmt"
+)
 
 func FindFirstOfSubString(parent, substring string) (int, int) {
 	return FindFirstOfSubStringWithStartingIndex(parent, substring, 0)
@@ -30,15 +35,93 @@ func FindIndicesBetweenRunes(parent string, leftRune, rightRune rune) (int, int)
 }
 
 func FindIndicesBetweenRunesWithStartingIndex(parent string, leftRune, rightRune rune, startingIndex int) (int, int) {
-	substring := parent[startingIndex:len(parent)]
+	substring := parent[startingIndex:]
 	lboundIndex := strings.IndexRune(substring, leftRune)
 	if lboundIndex > -1 {
 		rboundStartingPos := startingIndex + lboundIndex + 1
-		substring = parent[rboundStartingPos:len(parent)]
+		substring = parent[rboundStartingPos:]
 		rboundIndex := strings.IndexRune(substring, rightRune)
 		return lboundIndex + startingIndex, rboundIndex + rboundStartingPos
 	}
 	return -1, -1
+}
+
+func FindIndicesBetweenMatchingRunes(parent string, leftRune, rightRune rune, ignoreQuotsAndComments bool) (int, int) {
+	return FindIndicesBetweenMatchingRunesWithStartingIndex(parent, leftRune, rightRune, 0, ignoreQuotsAndComments)
+}
+
+func FindIndicesBetweenMatchingRunesWithStartingIndex(parent string, leftRune, rightRune rune, startingIndex int, ignoreQuotsAndComments bool) (int, int) {
+	substring := parent[startingIndex:]
+	foundLeftRune := false
+	lbounds, rbounds := -1, -1
+	counter := 0
+	isValidCode := true
+	var invalidationType string
+	for i, r := range substring {
+		//validate code block
+		if isValidCode {
+			if r == '/' && i < len(substring)-1 {
+				if substring[i+1] == '/' {
+					invalidationType = "//"
+					isValidCode = false
+					//println("single line comment found, ignoring next runes")
+					continue
+				} else if substring[i+1] == '*' {
+					invalidationType = "/*"
+					isValidCode = false
+					//println("multi line comment found, ignoring next runes")
+					continue
+				}
+			} else if r == '"' && i < len(substring)-1 {
+				invalidationType = "\""
+				isValidCode = false
+				//println("string text found, ignoring next runes")
+				continue
+			}
+		} else {
+			if invalidationType == "//" && r == '\n' {
+				isValidCode = true
+				invalidationType = ""
+				//println("single line comment ended, processing next runes")
+				continue
+			} else if invalidationType == "/*" && r == '/' {
+				if substring[i-1] == '*' {
+					isValidCode = true
+					invalidationType = ""
+					//println("multi line comment ended, processing next runes")
+					continue
+				}
+			} else if invalidationType == "\"" && r == '"' {
+				if substring[i-1] != '\\' {
+					isValidCode = true
+					invalidationType = ""
+					//println("string text ended, processing next runes")
+					continue
+				}
+			}
+		}
+		//don't process any invalid code if ignore bool has been set
+		if isValidCode == false && ignoreQuotsAndComments == true {
+			continue
+		}
+		if foundLeftRune {
+			if r == leftRune {
+				counter++
+			} else if r == rightRune {
+				counter--
+			}
+		}
+		if foundLeftRune == false && r == leftRune && isValidCode {
+			foundLeftRune = true
+			lbounds = i
+			counter++
+		}
+		if foundLeftRune && counter == 0 {
+			rbounds = i - 1
+			break
+		}
+	}
+	return lbounds + startingIndex, rbounds + startingIndex
 }
 
 func FindIndicesBetweenRunesContaining(parent string, leftRune, rightRune rune, substring string) (int, int) {
